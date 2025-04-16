77cc75c40fd885fd2c7526054b117ef5ea550799
jonasmajauskas <jonasmajauskas1@gmail.com>
2025-04-16 14:19:21 -0500

    button fix

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -38,6 +38,7 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
+
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
@@ -47,6 +48,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     );
   }
 );
+
 Button.displayName = "Button";
 
 export { Button, buttonVariants };
